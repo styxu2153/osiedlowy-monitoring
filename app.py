5b3c02df124a8883121a8db197adc833a37cb1e6
lukasz-test <97874045+lukasz-test@users.noreply.github.com>
2022-01-17 13:39:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    text = os.environ.get('XD', 'xD')
+    text = open('xd.txt').read()
     return render_template("index.html", text=text)
 
 if __name__=="__main__":
