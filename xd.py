54305259d04f05e1b0e85ddbf4534b92f64ca513
lukasz-test <97874045+lukasz-test@users.noreply.github.com>
2022-01-17 13:30:48 +0100

    Update xd.py

--- xd.py
+++ xd.py
@@ -6,29 +6,8 @@ config.read('settings.ini')
 
 domena = config['XD']['domena']
 
-
-
-#os.system('apt update')
-#os.system('apt upgrade')
-
-#os.system('apt install python3-pip python3-dev python3-venv build-essential libssl-dev libffi-dev python3-setuptools')
-#os.system('apt install nano')
-#os.system('apt remove apache2')
-#os.system('apt install nginx')
-#os.system('systemctl enable nginx')
-#os.system('systemctl start nginx')
-
-#os.system('rm -r /var/www/html')
-
-#os.system('
-#os.system('
-#os.system('
-#os.system('
-
-
-
 plik_nginxa = ''
-plik_nginxa_template = open('NAZWA_STRONY').readlines()
+plik_nginxa_template = open('nginx_file').readlines()
 for l in plik_nginxa_template:
 	if l.strip().startswith('server_name'):
 		l=l.replace('NAZWA_STRONY', domena)
@@ -40,7 +19,7 @@ with open("/etc/nginx/sites-available/{}".format(domena), "w") as file1:
 os.system('sudo ln -s /etc/nginx/sites-available/{} /etc/nginx/sites-enabled/'.format(domena))
 
 plik_gunicorn = open('flaga.service').read()
-with open('/etc/systemd/system/flaga.service2', "w") as f:
+with open('/etc/systemd/system/flaga.service', "w") as f:
 	f.write(plik_gunicorn)
 
 print(plik_nginxa)
